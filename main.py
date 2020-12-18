ca77f212337cc30c82de07f344e595343eb32a86
ConorAspell <conor.aspell@ucdconnect.ie>
2020-12-18 01:59:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import asyncio
 import os
 
 if __name__ == "__main__":
-    email="conor.aspell@ucdconnect.ie"
-    password="team_mattata"
-    user_id="4340827"
-    # email=os.environ.get('EMAIL')
-    # password=os.environ.get('PASSWORD')
-    # user_id=os.environ.get('USER_ID')
+    email=os.environ.get('EMAIL')
+    password=os.environ.get('PASSWORD')
+    user_id=os.environ.get('USER_ID')
     asyncio.run(update(email, password,user_id))
